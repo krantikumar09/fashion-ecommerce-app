1670a2a9e9f56bdbfeec7e5c406b9f40394bf717
Krantikumar Mathapati <krantikumar3446@gmail.com>
2024-10-31 16:36:36 +0530

    update main.js and ShopContext.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import ShopContextProvider from "./context/ShopContext.jsx";
+import { ShopContextProvider } from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
